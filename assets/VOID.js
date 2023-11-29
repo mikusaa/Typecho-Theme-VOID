bfd4725a6d09ea75a6dcbe9e80d31b7deb05d8be
mikusaa <mikusaaaaa@gmail.com>
2023-11-29 21:00:57 +0800

    添加代码框剪切板按钮

--- assets/VOID.js
+++ assets/VOID.js
@@ -624,3 +624,34 @@ var AjaxComment = {
         $('#uptime').html(days + ' 天 ' + hours + ' 小时 ' + minutes + ' 分 ' + seconds + ' 秒 ');
     }, 1000);
 })();
+
+// 复制处理函数
+function copyHandle(content, successMessage, errorMessage) {
+    navigator.clipboard.writeText(content)
+        .then(() => {
+            VOID.alert(successMessage || "复制成功");
+        })
+        .catch((error) => {
+            console.error(errorMessage || '复制失败:', error);
+            VOID.alert(errorMessage || "复制失败");
+        });
+}
+
+
+// 点击事件，通过事件委托处理
+function addClickListener() {
+    $('body').on('click', '.clipboard', function () {
+        copyHandle($(this).next().text());
+    });
+}
+
+function loadClipboard() {
+    // 在每个 <pre> 元素前添加带有复制图标的 div 元素
+    $('pre').prepend('<div class="clipboard"><svg aria-hidden="true" role="img" class="clipboard-icon" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display: inline-block; user-select: none; vertical-align: text-bottom;"><path fill-rule="evenodd" d="M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z"></path></svg></div>');
+    addClickListener();
+}
+
+// 在页面加载时设置事件
+$(document).ready(function () {
+    loadClipboard();
+});
