7e3247499168aad82ea4ce5a74ced0539aab90ad
mikusaa <mikusaa@users.noreply.github.com>
2026-08-15 07:14:28 +0000

    Automated deployment: Sat Aug 15 07:14:28 UTC 2026 49aa8acc71111e14a1c93a4cf3b4d54a324a21e1

--- includes/footer.php
+++ includes/footer.php
@@ -135,7 +135,7 @@ $setting = $GLOBALS['VOIDSetting'];
         }
         </script>
         <?php endif; ?>
-        <script data-manual src="<?php Utils::indexTheme('/assets/bundle-093dffbf5e.js'); ?>"></script>
+        <script data-manual src="<?php Utils::indexTheme('/assets/bundle-13c60e2ef2.js'); ?>"></script>
         <?php if($setting['enableMath']): ?>
         <script>
             window.MathJax = {
@@ -154,7 +154,7 @@ $setting = $GLOBALS['VOIDSetting'];
         </script>
         <script id="MathJax-script" src='<?php Utils::indexTheme('/assets/libs/mathjax/4.1.1/tex-svg.js'); ?>'></script>
         <?php endif; ?>
-        <script src="<?php Utils::indexTheme('/assets/VOID-c84cd1e9b8.js'); ?>"></script>
+        <script src="<?php Utils::indexTheme('/assets/VOID-08da307ccc.js'); ?>"></script>
         <?php if($setting['pjax']): ?>
         <script>
             $(document).on('pjax:complete', function(event, xhr, status, options){
