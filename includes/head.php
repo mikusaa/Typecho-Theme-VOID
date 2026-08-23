5f758c9d1db8c06e8ea22a9fba1139d010fb316e
mikusaa <mikusaa@users.noreply.github.com>
2026-08-23 05:25:01 +0000

    Automated deployment: Sun Aug 23 05:25:01 UTC 2026 64bed0bbdb16dc94d9e98d4915166da8c5c7bfba

--- includes/head.php
+++ includes/head.php
@@ -86,7 +86,7 @@ if (isset($_POST['void_action'])) {
 
     <!--CSS-->
     <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/bundle-45665c5cfe.css');?>">
-    <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/VOID-da5c7dba4a.css');?>">
+    <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/VOID-7ecd3f68f2.css');?>">
 
     <!--JS-->
     <script src="<?php Utils::indexTheme('/assets/bundle-header-8843e0e51a.js'); ?>"></script>
