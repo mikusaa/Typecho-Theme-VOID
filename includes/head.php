d34b9410c8f9078c73ad27e7ebd70b2f793cab32
mikusaa <mikusaa@users.noreply.github.com>
2026-08-31 13:21:47 +0000

    deploy: 544c63d77d9366528fd2b303ddfaaa65228a7272

--- includes/head.php
+++ includes/head.php
@@ -147,7 +147,7 @@ if (isset($_POST['void_action'])) {
         <link href="<?php Utils::indexTheme('/assets/fonts/fontsource/fira-code/5.3.0-r1/400.css'); ?>" rel="stylesheet">
     <?php endif; ?>
     <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/bundle-322b12e845.css');?>">
-    <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/VOID-8fc46a5626.css');?>">
+    <link rel="stylesheet" href="<?php Utils::indexTheme('/assets/VOID-d8467213b1.css');?>">
 
     <!--JS-->
     <script src="<?php Utils::indexTheme('/assets/bundle-header-1c2d9f3c9f.js'); ?>"></script>
