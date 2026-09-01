b189975bed90210c59f16bd67392219130031186
mikusaa <mikusaa@users.noreply.github.com>
2026-09-01 07:55:17 +0000

    deploy: c205bb918e7f1ead9ce4b17bf8656a87ad22ea01

--- includes/footer.php
+++ includes/footer.php
@@ -393,7 +393,7 @@ $setting = $GLOBALS['VOIDSetting'];
         }());
         </script>
         <script data-manual src="<?php Utils::indexTheme('/assets/bundle-bef316003c.js'); ?>"></script>
-        <script src="<?php Utils::indexTheme('/assets/VOID-b0e5f6cb4a.js'); ?>"></script>
+        <script src="<?php Utils::indexTheme('/assets/VOID-832c4f57e4.js'); ?>"></script>
         <?php if($setting['pjax']): ?>
         <script>
             $(document).on('pjax:complete', function(event, xhr, status, options){
