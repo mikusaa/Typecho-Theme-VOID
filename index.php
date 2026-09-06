3adeed0a7f3bea7998d12f7e1edc2260c3a0b81b
mikusaa <mikusaa@users.noreply.github.com>
2026-09-06 09:22:49 +0000

    deploy: f6a2c172aaaf92a280a4b071a3b8c48a1e7ad693

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * 
  * @package     Typecho-Theme-VOID
  * @author      熊猫小A
- * @version     4.0.0
+ * @version     4.0.0-beta.1
  * @link        https://blog.imalan.cn/archives/247/
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
